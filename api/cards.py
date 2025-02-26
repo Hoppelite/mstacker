559f367c52700f7557d871e4bb9ff564228958b4
sam <hoppelite@gmail.com>
2025-02-26 23:17:23 +1300

    Added some comments and types

--- api/cards.py
+++ api/cards.py
@@ -2,7 +2,7 @@ from api import handler
 from models.card import Card
 from uuid import UUID
 
-def get_card_by_name(name: str) -> any:
+def get_card_by_name(name: str) -> Card:
   url = handler.url_for("cards/named")
   params = {"fuzzy": name}
   response = handler.get(url, params)
@@ -13,7 +13,7 @@ def get_rulings_for_card(card: Card) -> any:
   response = handler.get(url)
   return response
 
-def get_card_by_id(id: UUID) -> any:
+def get_card_by_id(id: UUID) -> Card:
   url = handler.url_for("cards", id)
   response = handler.get(url)
   return Card(**response)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import api.cards as card_api
 
+# Example of how to use the card_api
+# The search is fuzzy, but it will 404 if it isn't specific enough
 card = card_api.get_card_by_name("Talion the kindly")
